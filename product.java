2f375312e156aa57e0ada898f367280ca9e9ff3b
Probin Bhagchandani <probinbhagchandani1509@gmail.com>
2025-05-18 18:40:40 +0530

    few changes were made

--- product.java
+++ product.java
@@ -3,11 +3,11 @@ import java.util.Queue;
 import java.time.*;
 import java.lang.*;
 import java.io.*;
-public class sum
+public class product
 {
      public static void main(String[] args)
      {
           Scanner sc=new Scanner(System.in);
-          System.out.println("Answer= " +(3+4));
+          System.out.println("Product= "+(3*5));
      }
 }
