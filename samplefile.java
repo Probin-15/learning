bfe225a9576869073d5db88e7b5616d52fc4f665
Probin Bhagchandani <probinbhagchandani1509@gmail.com>
2025-05-18 19:06:08 +0530

    I made some changes

---
+++ samplefile.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.Queue;
+import java.time.*;
+import java.lang.*;
+import java.io.*;
+public class samplefile
+{
+     public static void main(String[] args)
+     {
+          Scanner sc=new Scanner(System.in);
+          System.out.println("This is just a sample file which is made to test the working only.");
+     }
+}
