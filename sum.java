8bf7b0f687bfd1a4669353cd1f14125cc8d8cd84
Probin Bhagchandani <probinbhagchandani1509@gmail.com>
2025-05-18 18:36:40 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.Queue;
+import java.time.*;
+import java.lang.*;
+import java.io.*;
+public class sum
+{
+     public static void main(String[] args)
+     {
+          Scanner sc=new Scanner(System.in);
+          System.out.println(3+4);
+     }
+}
